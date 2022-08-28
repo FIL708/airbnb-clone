04f3d53274528b061fe830ce065058937e00a21a
FIL708 <filip.zebrowski4013@gmail.com>
2022-08-28 17:04:11 +0200

    rendering a card components from 'fetched data'

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import cardData from "./data"
 
 function App() {
+    const cardElements = cardData.map(data => (
+      <Card 
+        img={data.coverImg}
+        rating={data.stats.rating}
+        reviewCount={data.stats.reviewCount}
+        location={data.location}
+        title={data.title}
+        price={data.price}
+      />
+    ))
+
+
     return (
       <div className="container" >
         <Navbar />
         <Hero />
-        <Card 
-          img='katie-zaferes.png'
-          rating='5.0'
-          reviewCount={6}
-          location="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />
+        {cardElements}
       </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -17,7 +17,7 @@ export default function Card(props) {
                     className="card--star" />
                     <span>{props.rating}</span>
                     <span className="gray">({props.reviewCount}) • </span>
-                    <span className="gray">{props.country}</span>
+                    <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><strong>From ${props.price}</strong> / person</p>
